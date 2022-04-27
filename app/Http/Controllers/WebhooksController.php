5b412f5d836fb8da0443926eb7e21476c2c1d411
Garpepi H. Aotearoa <garpepi.aotearoa@gmail.com>
2022-04-27 17:45:34 +0700

    FIRST COMMIT

---
+++ app/Http/Controllers/WebhooksController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebhooksController extends Controller
+{
+    //
+    public function index() {
+        echo 'nothing to see here';
+    }
+
+    public function store() {
+
+    }
+}
